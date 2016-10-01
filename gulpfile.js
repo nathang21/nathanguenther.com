8a2a4cd833679a950682f8a8519e069a248412e8
Nathan Guenther <nathanpguenther@gmail.com>
2016-10-01 07:12:16 +0000

    Finish Tutorial http://david-barreto.com/working-with-sass-bootstrap-and-gulp/

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,58 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
+var wiredep = require('wiredep').stream;
 var del = require('del');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
 
 gulp.task('clean', function(){
   return del(['public']);
 });
 
 gulp.task('styles', ['clean'], function(){
-  return gulp.src('src/css/main.scss')
+  var injectAppFiles = gulp.src('src/css/*.scss', {read: false});
+  var injectGlobalFiles = gulp.src('src/global/*.scss', {read: false});
+
+  function transformFilepath(filepath) {
+    return '@import "' + filepath + '";';
+  }
+
+  var injectAppOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:app',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+
+  var injectGlobalOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:global',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+
+  return gulp.src('src/main.scss')
+    .pipe(wiredep())
+    .pipe(inject(injectGlobalFiles, injectGlobalOptions))
+    .pipe(inject(injectAppFiles, injectAppOptions))
     .pipe(sass())
+    .pipe(csso())
     .pipe(gulp.dest('public/css'))
 });
 
-gulp.task('default', ['clean', 'styles'], function(){
-  var injectFiles = gulp.src(['public/css/main.css']);
+gulp.task('vendors', function(){
+  return gulp.src(mainBowerFiles())
+    .pipe(filter('*.css'))
+    .pipe(concat('vendors.css'))
+    .pipe(csso())
+    .pipe(gulp.dest('public/css'))
+})
+
+gulp.task('default', ['clean', 'styles', 'vendors'], function(){
+  var injectFiles = gulp.src(['public/css/main.css', 'public/css/vendors.css']);
 
   var injectOptions = {
     addRootSlash: false,

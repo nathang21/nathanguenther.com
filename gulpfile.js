a61b2aacb73620412b69be1979b9ec4483b6bdf8
Nathan Guenther <nathanpguenther@gmail.com>
2016-10-02 18:14:44 +0000

    Minification HTML & CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,17 @@ var del = require('del');
 var mainBowerFiles = require('main-bower-files');
 var filter = require('gulp-filter');
 var concat = require('gulp-concat');
-var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+var htmlmin = require('gulp-htmlmin');
+
 
 gulp.task('clean', function(){
   return del(['public']);
 });
 
-gulp.task('styles', ['clean'], function(){
+
+gulp.task('css', ['clean'], function(){
   var injectAppFiles = gulp.src('src/css/*.scss', {read: false});
   var injectGlobalFiles = gulp.src('src/global/*.scss', {read: false});
 
@@ -39,27 +43,36 @@ gulp.task('styles', ['clean'], function(){
     .pipe(inject(injectGlobalFiles, injectGlobalOptions))
     .pipe(inject(injectAppFiles, injectAppOptions))
     .pipe(sass())
-    .pipe(csso())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('public/css'))
 });
 
-gulp.task('vendors', function(){
+
+gulp.task('vendors', ['clean'], function(){
   return gulp.src(mainBowerFiles())
     .pipe(filter('*.css'))
     .pipe(concat('vendors.css'))
-    .pipe(csso())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('public/css'))
 })
 
-gulp.task('default', ['clean', 'styles', 'vendors'], function(){
-  var injectFiles = gulp.src(['public/css/main.css', 'public/css/vendors.css']);
+
+gulp.task('html', ['clean', 'css', 'vendors'], function() {
+  var injectFiles = gulp.src(['public/css/main.min.css', 'public/css/vendors.css']);
 
   var injectOptions = {
     addRootSlash: false,
     ignorePath: ['src', 'public']
   };
 
+  //return gulp.src('src/html/*.html')
   return gulp.src('src/index.html')
     .pipe(inject(injectFiles, injectOptions))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('public'));
 });
+
+
+gulp.task('default', ['clean', 'css', 'vendors', 'html']);
